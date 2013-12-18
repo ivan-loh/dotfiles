a9a0a7f05a54c2aa6687d941a4b8a8207685d83c
Ivan Loh <ivan@eriad.com>
2013-12-18 11:44:46 +0800

    add auto complete

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -75,3 +75,19 @@
 (require 'jade-mode)    
 (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+;; ---------------------
+;; -- auto-complete   --
+;; ---------------------
+(add-to-list 'load-path "~/dotfiles/auto-complete")
+; Load the default configuration
+(require 'auto-complete-config)
+; Make sure we can find the dictionaries
+(add-to-list 'ac-dictionary-directories "~/emacs/auto-complete/dict")
+; Use dictionaries by default
+(setq-default ac-sources (add-to-list 'ac-sources 'ac-source-dictionary))
+(global-auto-complete-mode t)
+; Start auto-completion after 2 characters of a word
+(setq ac-auto-start 2)
+; case sensitivity is important when finding matches
+(setq ac-ignore-case nil)
